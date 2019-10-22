beae041aa990c41085d4933151bddbef2f32cdb6
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-10-22 15:13:28 +0900

    Update api

--- src/App.js
+++ src/App.js
@@ -1,77 +1,24 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
-import noteService from './services/notes'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
+import noteService from './services/notes' 
 
-const Notification = ({ message }) => {
-  if (message === null) {
-    return null
-  }
-
-  return (
-    <div className="error">
-      {message}
-    </div>
-  )
-}
-
-const Footer = () => {
-  const footerStyle = {
-    color: 'green',
-    fontStyle: 'italic',
-    fontSize: 16
-  }
-
-  return (
-    <div style={footerStyle}>
-      <br/>
-      <em>Note app, Department of Computer Science, University of Helsinki 2019</em>
-    </div>
-  )
-}
-
-
-const App = (props) => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
+const App = () => {
+  const [notes, setNotes] = useState([]) 
+  const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
       .getAll()
-      .then(initialNotes => {
-        setNotes(initialNotes)
-      })
+      .then(initialNotes => setNotes(initialNotes))
   }, [])
 
-  console.log('render', notes.length, 'notes')
-
-  const toggleImportanceOf = id => {
-    //notes全体から引数に指定されたnoteを探す。
-    const note = notes.find(n => n.id === id)
-    const changeNote = { ...note, important: !note.important}
-
-    noteService
-      .update(id, changeNote).then(returnedNote => {
-        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-      })
-      .catch(error => {
-        setErrorMessage(
-          `Note '${note.content}' was already removed from server`
-        )
-        setTimeout(() => {
-          setErrorMessage(null)
-        },5000)
-        setNotes(notes.filter(n => n.id !== id))
-      })
-    console.dir(changeNote);
-    console.log(`importance of ${id} needs to be toggled`)
-  }
-
   const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important === true)
+    ? notes
+    : notes.filter(note => note.important)
 
   const rows = () => notesToShow.map(note =>
     <Note
@@ -80,34 +27,57 @@ const App = (props) => {
       toggleImportance={() => toggleImportanceOf(note.id)}
     />
   )
+  
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
 
-  const addNote = event => {
+  const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      data: new Date(),
+      date: new Date().toISOString(),
       important: Math.random() > 0.5,
+      id: notes.length + 1,
     }
+
     noteService
       .create(noteObject)
-      .then(returnedNote => {
-        setNotes(notes.concat(returnedNote))
+      .then(data => {
+        setNotes(notes.concat(data))
         setNewNote('')
       })
   }
 
-  const handleNoteChange = (event) =>{
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
 
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+      
+  }
 
   return (
     <div>
       <h1>Notes</h1>
+
       <Notification message={errorMessage} />
+
       <div>
-        <button onClick={() => setShowAll(!showAll)}> 
+        <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
         </button>
       </div>
@@ -115,12 +85,16 @@ const App = (props) => {
         {rows()}
       </ul>
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange}/>
-        <button type="submit">Save</button>
+        <input
+          value={newNote} 
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
       </form>
-      <Footer/>
+
+      <Footer />
     </div>
   )
 }
 
-export default App
+export default App 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import axios from 'axios'
-import "./index.css";
+import App from './App'  
+import './index.css'
 
-axios.get('http://localhost:3001/notes').then(response => {
-    const notes = response.data
-    ReactDOM.render(
-      <App notes={notes} />,
-      document.getElementById('root')
-    )
-})
-
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
